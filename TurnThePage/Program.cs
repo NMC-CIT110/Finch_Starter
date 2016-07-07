a565b9dc7f4105b8744999ae9433cad503a3460f
John Velis <john@johnvelis.com>
2016-07-07 07:24:02 -0400

    Initial Commit

---
+++ TurnThePage/Program.cs
@@ -0,0 +1,35 @@
+﻿using FinchAPI;
+using System;
+
+namespace Resource_Finch_StarterSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // create a new Finch object
+            //
+            Finch myFinch;
+            myFinch = new Finch();
+
+            //
+            // call the connect method
+            //
+            myFinch.connect();
+            
+            //
+            // begin your code
+            //
+
+            //
+            //end of your code
+            //
+
+            //
+            // call the disconnect method
+            //
+            myFinch.disConnect();
+        }
+    }
+}
